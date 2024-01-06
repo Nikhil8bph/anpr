c5b7e899c711140766109ba4f457cba04fc762ce
Nikhil8bph <nikhil8.bph@gmail.com>
2024-01-06 19:23:27 +0530

    plate detection and wrap transformation

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from ultralytics import YOLO
 import cv2
 import easyocr
-from utils import get_car, read_plate
-# from sort.sort import *
+from utils import get_car, read_plate, wrap_transform_plate
+from wrappodnet.wpodnet.model import WPODNet
+import torch
 
 #load models
 results = {}
 vehicles = [2, 3, 5, 7]
 vehicle_detector = YOLO('./models/yolov8n.onnx',task='detect')
-# mot_tracker = Sort()
+
 license_plate_detector = YOLO('./models/license_plate_detector.onnx',task='detect')
 reader = easyocr.Reader(['en'], gpu=False)
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+wpodnet_model = WPODNet()
+wpodnet_model.to(device)
+wpodnet_checkpoint = torch.load('./models/wpodnet.pth')
+wpodnet_model.load_state_dict(wpodnet_checkpoint)
 
 # load video
 cap = cv2.VideoCapture('./test-data/sample.mp4')
@@ -25,15 +32,14 @@ while ret:
         results[frame_nmr] = {}
         # detect vehicles
         vehicle_detections_ = vehicle_detector.track(frame,classes=vehicles,persist=True)[0]
-                   
         plate_detections_ = license_plate_detector(frame)[0]
         for plate_detection in plate_detections_.boxes.data.tolist():
             vehicle_plate = get_car(plate_detection, vehicle_detections_)
             vehicle_x1, vehicle_y1, vehicle_x2, vehicle_y2, vehicle_track_id, vehicle_score, vechile_class_id = vehicle_plate
             if vehicle_x1>0 or vehicle_y1>0 or vehicle_x2>0 or vehicle_y2>0:
                 plate_x1, plate_y1, plate_x2 ,plate_y2 ,plate_score ,plate_class_id = plate_detection
-                license_plate_crop = frame[int(plate_y1):int(plate_y2), int(plate_x1): int(plate_x2)]
-                plate_text, plate_text_score = read_plate(reader, license_plate_crop)
+                wrapped_plate = wrap_transform_plate(frame[int(vehicle_y1):int(vehicle_y2), int(vehicle_x1): int(vehicle_x2)], wpodnet_model)
+                plate_text, plate_text_score = read_plate(reader, wrapped_plate)
                 plate_org = (int(plate_x1), int(plate_y1) - 10)
                 cv2.rectangle(frame, (int(plate_x1), int(plate_y1)), (int(plate_x2), int(plate_y2)), (0, 255, 0), 2)
                 cv2.putText(frame, plate_text, plate_org, cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 255, 255), 1)
@@ -44,6 +50,7 @@ while ret:
                                                                     'text_score': plate_text_score},
                                                         }
                 
+                                
         for vehicle_detection in vehicle_detections_.boxes.data.tolist():
             vehicle_x1, vehicle_y1, vehicle_x2, vehicle_y2, vehicle_track_id, vehicle_score, vechile_class_id = vehicle_detection
             car_org = (int(vehicle_x1), int(vehicle_y1) - 10)
--- utils.py
+++ utils.py
@@ -1,3 +1,10 @@
+import torch
+import cv2
+from wrappodnet.wpodnet.backend import Predictor
+from wrappodnet.wpodnet.model import WPODNet
+from wrappodnet.wpodnet.stream import ImageStreamer
+from PIL import Image
+import numpy as np
 
 def get_car(plate_detection,vehicle_detections_):
     plate_x1, plate_y1, plate_x2 ,plate_y2 ,plate_score ,plate_class_id = plate_detection
@@ -14,3 +21,12 @@ def read_plate(reader, license_plate_crop):
         text = text.upper().replace(' ', '')
         return text, score
     return "text_not_clear",-1
+
+def wrap_transform_plate(source, wpodnet_model, scale = 1.0):
+    cropped_image = cv2.cvtColor(source, cv2.COLOR_BGR2RGB)
+    predictor = Predictor(wpodnet_model)
+    prediction = predictor.predict(Image.fromarray(cropped_image), scaling_ratio=scale)
+    warped = prediction.warp()
+    numpy_image = np.array(warped)
+    warped_image = cv2.cvtColor(numpy_image, cv2.COLOR_RGB2BGR)
+    return warped_image
